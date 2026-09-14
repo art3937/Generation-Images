ef87fa5cd1fc94b67e7ac05581f0208a1d435138
Artemii Telegin <art3937@gmail.com>
2026-09-14 22:49:49 +0300

    isMinifyEnabled = false

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
